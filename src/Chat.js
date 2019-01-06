21688a3dc79ec7f10273fe2e9ec0224a5ec5b9b4
Guillem <guillem.sannicolas@keradgames.com>
2019-01-06 21:14:18 +0100

    Input handling

--- src/Chat.js
+++ src/Chat.js
@@ -6,19 +6,78 @@ export class Chat extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            messages: [{Left:"Hola que tal?", Timestamp:"whatever"}, {Right:"Muy bien", Timestamp:"whatever"}],
+            messages: [],
             names: ["Left", "Right"],
+            left: "",
+            right: "",
         };
+        this.addValueLeft = this.addValueLeft.bind(this);
+        this.updateInputLeft = this.updateInputLeft.bind(this);
+
+        this.addValueRight = this.addValueRight.bind(this);
+        this.updateInputRight = this.updateInputRight.bind(this);
     }
 
     send(i)
     {
         const messages = this.state.messages;
         const names = this.state.names;
+        if(i==0)
+        {
+            this.setState({
+                messages: messages.concat({Left: this.state.left}),
+                names: names,
+                left: ""
+            });
+        }
+        else
+        {
+            this.setState({
+                messages: messages.concat({Right: this.state.right}),
+                names: names,
+                right: ""
+            });
+        }
+    }
+
+    addValueLeft(evt)
+    {
+      evt.preventDefault();
+      if(this.state.left != "")
+      {
+        this.send(0);
+        evt.target.reset();
+      }
+      return false;
+    }
+
+    updateInputLeft(evt){
         this.setState({
-            messages: messages.concat({Left: "New message"}),
-            names: names
-        });
+            messages: this.state.messages,
+            names: this.state.names,
+            right: this.state.right,
+            left: evt.target.value
+        });   
+    }
+
+    addValueRight(evt)
+    {
+      evt.preventDefault();
+      if(this.state.right != "")
+      {
+        evt.target.reset();
+        this.send(1);
+      }
+      return false;
+    }
+
+    updateInputRight(evt){
+        this.setState({
+            messages: this.state.messages,
+            names: this.state.names,
+            right: evt.target.value,
+            left: this.state.left
+        });   
     }
 
     render() {
@@ -37,10 +96,16 @@ export class Chat extends React.Component {
                     />
                 </div>
                 <div className="input-field">
-                    <button className="send-button" onClick={() => this.send(0)}>Send</button>
+                    <form onSubmit={this.addValueLeft}>
+                        <input className="input-field-text" type="text" onChange={this.updateInputLeft} /><br/><br/>
+                        <input className="send-button" type="submit" value="Send"/>
+                    </form>
                 </div>
                 <div className="input-field">
-                    <button className="send-button" onClick={() => this.send(1)}>Send</button>
+                    <form onSubmit={this.addValueRight}>
+                        <input className="input-field-text" type="text" onChange={this.updateInputRight} /><br/><br/>
+                        <input className="send-button" type="submit" value="Send"/>
+                    </form>
                 </div>
             </div>
         );
